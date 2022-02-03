d803bf0c52357962ded1fec0801245fce88f0f35
GALHA <totogalha@gmail.com>
2022-02-03 01:34:49 -0300

    Handle Errors

--- galhafy/src/App.jsx
+++ galhafy/src/App.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./Components/MainPage";
+import { Route, Switch } from "wouter";
 
-import { Router, Route } from "wouter";
 import SearchPage from "./Components/SearchPage";
+import Footer from "./Components/Footer";
+import ErrorStatus from "./Components/ErrorStatus";
+import NotFoundGif from "./img/404.gif";
 
 function App() {
   return (
+    // esto es raro no  lo hagas aqui el layout
     <div className="w-2/4 p-10 mx-auto mt-10 font-mono text-white border-2 border-pink-500 rounded App bg-mainColor">
-      <Router>
+      <Switch>
         <Route path="/" component={MainPage}></Route>
         <Route
           path="/search/:search"
           component={(props) => <SearchPage searchTopic={props} />}
-        ></Route>
-      </Router>
+        />
+        <Route>
+          <ErrorStatus title={"404"} src={NotFoundGif} />
+        </Route>
+      </Switch>
 
-      <h6 className="mt-10 text-left">
-        Created with ♥ by
-        <a href="https://github.com/G4LH4" target="_BLANK" rel="noreferrer">
-          G4LH4
-        </a>
-      </h6>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// espera que primero le hago un commit XD
